8bf73ae7151afe929d42eb844066ab6c7d970fef
salestrekk_admin <sysadmin@salestrekk.com>
2020-05-20 10:02:12 +0700

    viet hoa gio hang trong

--- wp-content/plugins/woocommerce/templates/cart/mini-cart.php
+++ wp-content/plugins/woocommerce/templates/cart/mini-cart.php
@@ -89,7 +89,7 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 
 <?php else : ?>
 
-	<p class="woocommerce-mini-cart__empty-message"><?php esc_html_e( 'No products in the cart.', 'woocommerce' ); ?></p>
+	<p class="woocommerce-mini-cart__empty-message">Không có sản phẩm trong giỏ hàng!</p>
 
 <?php endif; ?>
 
--- wp-content/themes/antomi/woocommerce/cart/mini-cart.php
+++ wp-content/themes/antomi/woocommerce/cart/mini-cart.php
@@ -102,7 +102,7 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 
 		<?php else : ?>
 
-			<p class="woocommerce-mini-cart__empty-message"><?php _e( 'No products in the cart.', 'antomi' ); ?></p>
+			<p class="woocommerce-mini-cart__empty-message">Không có sản phẩm trong giỏ hàng!</p>
 
 		<?php endif; ?>
 
