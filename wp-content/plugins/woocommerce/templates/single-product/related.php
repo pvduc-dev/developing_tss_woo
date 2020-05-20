40292739edf54335d06100cf36aa9224253b4cba
salestrekk_admin <sysadmin@salestrekk.com>
2020-05-20 16:13:20 +0700

    edit

--- wp-content/plugins/woocommerce/templates/single-product/related.php
+++ wp-content/plugins/woocommerce/templates/single-product/related.php
@@ -28,7 +28,7 @@ if ( $related_products ) : ?>
 
 		if ( $heading ) :
 			?>
-			<h2><?php echo esc_html( $heading ); ?></h2>
+			<h2>Sản phẩm liên quan</h2>
 		<?php endif; ?>
 		
 		<?php woocommerce_product_loop_start(); ?>
